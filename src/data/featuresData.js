3508a730d2515f62d59f7e537666a0b09d7c4d60
june <silvea017@gmail.com>
2025-11-12 14:02:22 +0900

    20251112
    이미지 수정

--- src/data/featuresData.js
+++ src/data/featuresData.js
@@ -6,7 +6,7 @@ export const featuresData = [
       "영진 IT교육은 학생 개개인의 학습 진도와 수준을 꼼꼼히 관리하는 맞춤형 학습 관리 시스템을 제공합니다.",
     subDescription:
       "또한 포트폴리오 준비부터 면접 대비까지 현업 멘토의 1:1 맞춤 지도를 통해 취업 지원 프로그램을 운영합니다.",
-    imageSrc: "/img/img (1).jpg",
+    imageSrc: "/img/img (5).png",
     imagePosition: "right",
     backgroundColor: "bg-gray-100",
   },
@@ -17,7 +17,7 @@ export const featuresData = [
       "영진 IT교육의 커리큘럼은 최신 업계 트렌드를 실시 반영한 실무 기술을 바탕으로 현장에서 통하는 실무 역량을 기를 수 있습니다.",
     subDescription:
       "또한 프로젝트 중심 학습으로 실무에서의 역량을 체크해 취업 성공률을 높입니다.",
-    imageSrc: "/img/img (2).jpg",
+    imageSrc: "/img/img (6).png",
     imagePosition: "left",
     backgroundColor: "bg-gray-100",
   },
@@ -28,7 +28,7 @@ export const featuresData = [
       "국비 전액 지원으로 교육비 부담 없이 누구나 양질의 교육을 학습할 수 있습니다.",
     subDescription:
       "또한 훈련 장려금으로 월 최대 70만원까지 지급되어 학업과 생활을 동시에 이어갈 수 있습니다.",
-    imageSrc: "/img/img (3).jpg",
+    imageSrc: "/img/img (7).png",
     imagePosition: "right",
     backgroundColor: "bg-gray-100",
   },
